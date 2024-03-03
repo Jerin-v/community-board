b119f6534794b95e8f15ba11a771b0631b57bab8
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-03 16:13:50 -0600

    test of card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css';
+import Card from './components/Card';
 
 const App = () => {
 
   return (
-    <div className="App">
-
+    <div className="container">
+      <h1>World's Best Vacation Spots!</h1>
+      <Card 
+          img = 'src/assets/Borabora.jpeg'
+          title = 'Cuba'
+          desc = 'The small island of Bora Bora (just about 6 miles long and a little more than 2 miles wide) overflows with beauty. A dormant volcano rises up at its center and fans out into lush jungle before spilling into an aquamarine lagoon.'
+          />
       
     </div>
   )
